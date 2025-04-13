891f697043fba8f92a5fe478c12d489c73e855db
savemylife1209 <savemylife1209@outlook.com>
2025-04-13 18:57:49 +0700

    commit

--- bot.py
+++ bot.py
@@ -3,28 +3,25 @@ from discord.ext import commands
 from modules.helpers import *
 import os
 import asyncio
-from flask import Flask
 
-app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "MyBot is ready"
+client = commands.Bot(
+    command_prefix=PREFIX,
+    owner_ids=OWNER_IDS,
+    intents=discord.Intents.all()
+)
 
-async def main():
-    client = commands.Bot(
-        command_prefix=PREFIX,
-        owner_ids=OWNER_IDS,
-        intents=discord.Intents.all()
-    )
+client.remove_command('help')
 
-    client.remove_command('help')
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user}')
 
-    for filename in os.listdir(COG_FOLDER):
-        if filename.endswith('.py'):
-            await client.load_extension(f'cogs.{filename[:-3]}')
+for filename in os.listdir(COG_FOLDER):
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}')
 
-    await client.start(TOKEN)
+client.run(TOKEN)
 
 if __name__ == "__main__":
     asyncio.run(main())
